893d085cf25be12d24bb223ad8c9718c68d401e6
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-11-14 23:18:46 +0000

    Added prototype for struct and builtin functions

--- holberton.h
+++ holberton.h
@@ -1,6 +1,16 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
+/**
+ *
+ */
+
+typedef struct op
+{
+	char *command;
+	void(*f)(char *buff, char **commands);
+} built;
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -11,5 +21,7 @@
 
 char *getlinefxn();
 char **tokenfxn(char *buff);
+int _strcmp(char *s1, char *s2);
+void exitfxn(char *buff, char **commands);
 
 #endif
