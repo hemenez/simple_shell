64f518684cdc1377cb453e841f372d55046cf0dd
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-11-14 05:16:14 +0000

    Separated helper function from main file

---
+++ linestore.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * getlinefxn - function will get line and store in a buffer
+ * Return: returns pointer to buffer
+ */
+
+char *getlinefxn()
+{
+	char *buff = NULL;
+	size_t buffsize = 0;
+	ssize_t x;
+
+	x = getline(&buff, &buffsize, stdin);
+	if (buff == NULL)
+	{
+		perror("Error");
+		return (NULL);
+	}
+	if (x == -1)
+	{
+		free(buff);
+		return (NULL);
+	}
+	return (buff);
+}
