9b718c571a4a904a021cd81dc9b72b264bfe9efb
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-11-14 23:21:46 +0000

    Added comments for clarity

--- exitfxn.c
+++ exitfxn.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * exitfxn - function will exit out of terminal
+ * @buff: represents buffer holding line of commands
+ * @commands: represents pointer pointing to tokenized commands
+ * Return: function returns void
  */
 
 void exitfxn(char *buff, char **commands)
