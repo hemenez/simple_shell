f82bcc2f31860b1d1b5c134f9dcbcc83a456d0da
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-11-14 04:39:05 +0000

    Broke up single function into three smaller functions

--- shell.c
+++ shell.c
@@ -6,75 +6,101 @@
 #include <sys/wait.h>
 #define DELIM " \t\r\n\a"
 /**
- * main - function will replicate shell system
- * Return: function returns zero upon success, one on error
+ * getlinefxn - function will get line and store in a buffer
+ * Return: returns pointer to buffer
+ */
+
+char *getlinefxn()
+{
+	char *buff = NULL;
+	size_t buffsize = 0;
+	ssize_t x;
+
+	x = getline(&buff, &buffsize, stdin);
+	if (buff == NULL)
+	{
+		perror("Error");
+		return (NULL);
+	}
+	if (x == -1)
+	{
+		free(buff);
+		return (NULL);
+	}
+	return (buff);
+}
+
+/**
+ * tokenfxn - function will tokenize line
+ * @buff: represents pointer to buffer holding line
+ * Return: function returns pointer to array
+ */
+
+char **tokenfxn(char *buff)
+{
+	char **commands;
+	size_t index;
+	char *token;
+
+	commands = malloc(sizeof(char *) * 2);
+	if (commands == NULL)
+	{
+		free(buff);
+		return (NULL);
+	}
+	token = strtok(buff, DELIM);
+	commands[0] = token;
+	index = 1;
+	while (token != NULL)
+	{
+		commands = realloc(commands, (sizeof(char *) * (index + 2)));
+		if (commands == NULL)
+		{
+			free(buff);
+			return (NULL);
+		}
+		token = strtok(NULL, DELIM);
+		commands[index] = token;
+		index++;
+	}
+	commands[index] = NULL;
+	return (commands);
+}
+/**
+ * main - function will handle most function calls
+ * Return: function will always return 0
  */
 
 int main(void)
 {
-	char *commands[7];
+	char *buff;
+	char **commands;
 	pid_t pid;
 	int status;
-	ssize_t x;
-	int index;
-	char *token;
-	size_t buffsize = 0;
-	char *buff = NULL;
 
 	while (1)
 	{
 		printf("TalkToMe$ ");
-		x = getline(&buff, &buffsize, stdin);
-		if (buff == NULL)
-		{
-			perror("Error");
-			return (1);
-		}
-		if (x == -1)
-		{
-			free(buff);
-			return (1);
-		}
-		token = strtok(buff, DELIM);
-		index = 0;
-		while (token != NULL)
-		{
-			commands[index] = token;
-			token = strtok(NULL, DELIM);
-			index++;
-		}
-		commands[index] = NULL;
+		buff = getlinefxn();
+		commands = tokenfxn(buff);
 		pid = fork();
 		if (pid == -1)
-		{
 			perror("Error:");
-
-			return (1);
-		}
 		if (pid == 0)
 		{
 			if (commands[0] == NULL)
-			{
 				break;
-			}
-			/* Child process */
 			execve(commands[0], commands, NULL);
-			/* Only reaches if error executing */
 			perror("Error");
 		}
 		else if (pid > 0)
 		{
-			/* Parent process */
 			pid = wait(&status);
 			if (pid < 0)
-			{
 				perror("wait");
-				free(buff);
-				exit(1);
-			}
 		}
 	}
-	free(token);
 	free(buff);
+	free(commands);
 	return (0);
 }
