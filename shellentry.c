3306a9f675e0e1d078da84dda3d1a2b8291f3ab0
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-11-15 18:49:43 +0000

    Separated main function into two and fixed loop bug

--- shellentry.c
+++ shellentry.c
@@ -1,5 +1,39 @@
 #include "holberton.h"
 /**
+ * builtinfxn - function will see if input is a shell builtin
+ * @buff: represents buffer to be read
+ * @commands: represents commands to be analyzed
+ * Return: returns zero if builtin, one if not
+ */
+
+int builtinfxn(char *buff, char **commands)
+{
+	int i, j;
+	built in[] = {
+		{"exit", exitfxn},
+		{"env", envfxn},
+		{NULL, NULL},
+	};
+
+	i = 0;
+	while (commands != NULL && commands[i] != '\0')
+	{
+		j = 0;
+		while (in[j].command != NULL)
+		{
+			if (_strcmp(commands[i], (in[j].command)) == 0)
+			{
+				(in[j].f)(buff, commands);
+				return (0);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+/**
  * main - function will handle most function calls
  * Return: function will always return 0
  */
@@ -9,32 +43,16 @@ int main(void)
 	char *buff;
 	char **commands;
 	pid_t pid;
-	int status, i, j;
-	built in[] = {
-		{"exit", exitfxn},
-		{"env", envfxn},
-		{NULL, NULL},
-	};
+	int status, x;
 
 	while (1)
 	{
 		write(STDOUT_FILENO, "TalkToMe$ ", 11);
 		buff = getlinefxn();
 		commands = tokenfxn(buff);
-		i = 0;
-		while (commands != NULL && commands[i] != '\0')
-		{
-			j = 0;
-			while (in[j].command != NULL)
-			{
-				if (_strcmp(commands[i], (in[j].command)) == 0)
-				{
-					(in[j].f)(buff, commands);
-				}
-				j++;
-			}
-			i++;
-		}
+		x = builtinfxn(buff, commands);
+		if (x == 0)
+			continue;
 		pid = fork();
 		if (pid == -1)
 			perror("Error:");
