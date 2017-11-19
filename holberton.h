34d0876481b3a00ca1bb10463a679f511025ff82
Lindsey Hemenez <lindsey.hemenez@gmail.com>
2017-11-19 00:31:48 +0000

    Added function prototypes

--- holberton.h
+++ holberton.h
@@ -14,6 +14,7 @@ typedef struct op
 } built;
 
 #include <stdio.h>
+#include <sys/stat.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -29,5 +30,15 @@ int _strcmp(char *s1, char *s2);
 void exitfxn(char *buff, char **commands);
 void envfxn(char *buff, char **commands);
 int builtinfxn(char *buff, char **commands);
+
+
+char *_strcpy(char *dest, char *src);
+char *_strdup(char *str);
+int _strcmp(char *s1, char *s2);
+char *getpath(char *commands);
+char *_strstr(char *haystack, char *needle);
+char *_strncpy(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
